ab6980194aead681c9e4980d862434380061531c
kelemzol <kelemzol@elte.hu>
2016-01-09 23:58:25 +0100

    deriveStrucTrav error on [] type

--- src/Data/StructuralTraversal/Instances.hs
+++ src/Data/StructuralTraversal/Instances.hs
@@ -1,10 +1,21 @@
 {-# LANGUAGE FlexibleInstances
-           , UndecidableInstances #-}
+           , UndecidableInstances
+           , TemplateHaskell
+           #-}
 module Data.StructuralTraversal.Instances where
 
 import Data.StructuralTraversal.Class
 import Control.Applicative
 import Data.Traversable
- 
+import Data.StructuralTraversal.TH
+
 -- instance {-# OVERLAPPABLE #-} Traversable f => SmartTrav f where
   -- smartTrav desc asc f = traverse (smartTrav desc asc f)
+
+data List a = Nil | Cons a (List a)
+
+deriveStructTrav ''Maybe
+deriveStructTrav ''List
+deriveStructTrav ''[]
+
+
